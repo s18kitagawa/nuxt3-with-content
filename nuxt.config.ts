b54758f440526c1c1bd28410ea58001203dfd3f0
masahiro <36883559+s18kitagawa@users.noreply.github.com>
2023-08-30 18:08:35 +0900

    update : config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,12 @@
+import {
+  baseName,
+  baseDesc,
+  authorName,
+  snsTwitter,
+  snsFb,
+  baseUrl
+} from './utils/constants'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
@@ -5,6 +14,93 @@ export default defineNuxtConfig({
     '@zadigetvoltaire/nuxt-gtm', // https://github.com/zadigetvoltaire/nuxt-gtm
     'nuxt-jsonld' // https://github.com/ymmooot/nuxt-jsonld
   ],
+
+  app: {
+    head: {
+      htmlAttrs: { lang: 'ja', prefix: 'og: http://ogp.me/ns#' },
+      titleTemplate: '%s | ' + baseName, // https://nuxt.com/docs/getting-started/seo-meta#title-template
+
+      meta: [
+        { charset: 'utf-8' },
+        { 'http-equiv': 'x-ua-compatible', content: 'ie=edge' },
+        {
+          hid: 'viewport',
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1, user-scalable=no'
+        },
+        { hid: 'robots', name: 'robots', content: 'index, follow' },
+
+        { hid: 'author', name: 'author', content: authorName },
+        { hid: 'og:site_name', property: 'og:site_name', content: baseName },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:locale', property: 'og:locale', content: 'ja_JP' },
+        { hid: 'og:title', property: 'og:title', content: baseName },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: baseDesc
+        },
+        { hid: 'og:url', property: 'og:url', content: baseUrl },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: '/images/ogp/ogp_common.png'
+        },
+
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:site', content: snsTwitter },
+
+        { property: 'fb:app_id', content: snsFb }
+      ],
+
+      link: [
+        { rel: 'canonical', href: baseUrl },
+
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/images/favicons/favicon.ico'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/images/favicons/favicon-icon-16x16.png'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/images/favicons/favicon-icon-32x32.png'
+        },
+        {
+          rel: 'apple-touch-icon',
+          type: 'image/png',
+          sizes: '180x180',
+          href: '/images/favicons/apple-touch-icon.png'
+        },
+
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Open+Sans:wght@400;700&display=swap',
+          as: 'style'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Open+Sans:wght@400;700&display=swap',
+          media: 'print',
+          onload: "this.media='all'"
+        }
+      ]
+    }
+  },
+
   css: ['@/assets/styles/main.sass'],
   vite: {
     css: {
