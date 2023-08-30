4a308e26ed7159c52faf9a0a1cd1b13b5af5fbf8
masahiro <36883559+s18kitagawa@users.noreply.github.com>
2023-08-30 17:48:43 +0900

    add : Sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,18 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content' // https://content.nuxtjs.org/
   ],
+  css: ['@/assets/styles/main.sass'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: `
+          @use "./assets/styles/app.sass" as *
+          ` // active global Sass function
+        }
+      }
+    }
+  },
 
   devtools: {
     enabled: true
