283aa3f9e32f4e79a06c8f1738a2bb873893a395
masahiro <36883559+s18kitagawa@users.noreply.github.com>
2023-08-30 20:05:02 +0900

    update :pages & component

---
+++ components/organisms/GlobalNav.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav>
+    <span class="material-symbols-rounded">
+      menu
+    </span>
+  </nav>
+</template>
+
+<style lang="sass" scoped>
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
 
 <template>
   <div>
+    <GlobalNav />
     <NuxtPage />
   </div>
 </template>
