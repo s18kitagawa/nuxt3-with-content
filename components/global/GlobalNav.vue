0c3d79326c0c4eb9568a35c7ef9835d7eec57b23
masahiro <36883559+s18kitagawa@users.noreply.github.com>
2023-08-31 22:54:14 +0900

    update : components

--- components/global/GlobalNav.vue
+++ components/global/GlobalNav.vue
@@ -6,7 +6,7 @@ const globalNavItems = ref([
     txt: 'Home'
   },
   {
-    path: '/',
+    path: '/#works',
     icon: 'palette',
     txt: 'Works'
   },
@@ -56,10 +56,18 @@ const globalNavItem = computed(() => globalNavItems.value)
 <style lang="sass" scoped>
 .global-nav
   position: fixed
+  top: unset
+  left: unset
+  right: calc( 20px + 72px )
+  bottom: calc( 72px + 40px)
+  @include min-width($xl)
+    top: 0
+    left: 0
+    right: unset
+    bottom: unset
+
   &__menu
     position: absolute
-    top: 0
-    left: calc( 50vw - 1200px / 2 - 40px )
     background: $white
     border-radius: 50%
     width: 72px
@@ -67,7 +75,7 @@ const globalNavItem = computed(() => globalNavItems.value)
     display: flex
     justify-content: center
     align-items: center
-    transition: 0.2s
+    @include transition(0.2s)
     &:hover
       color: CSStoRGBA( '--color-link', 1)
       background: CSStoRGBA( '--color-link-low', 1)
@@ -75,20 +83,35 @@ const globalNavItem = computed(() => globalNavItems.value)
       background: CSStoRGBA( '--color-bg-middle', 1)
       &:hover
         color: CSStoRGBA( '--color-link', 1)
-        background: $sky-200
+    @include min-width($xl)
+      top: 80px
+      left: calc( 50vw - var(--scale-content) / 2 - 96px )
+      right: unset
+      bottom: unset
   &__menu-list
-    position: relative
-    top: calc(72px + 24px)
-    left: calc( 50vw - 1200px / 2 - 40px + 72px / 2 )
+    position: absolute
+    top: unset
+    left: unset
+    right: calc( -72px / 2 )
+    bottom: 0
+    visibility: hidden
     opacity: 0
-    transform: translateX( -40px )
-    transition: 0.3s
+    transform: translateX( 40px )
+    @include transition()
+    @include min-width($xl)
+      transform: translateX( -40px )
+      top: calc( var(--scale-8) + 72px + 24px)
+      left: calc( 50vw - var(--scale-content) / 2 - 96px + 72px / 2 )
+      right: unset
+      bottom: unset
     &.is-active
+      visibility: visible
       opacity: 1
       transform: translateX( 0px )
     &__item
       margin-bottom: 24px
       & a
+        color: CSStoRGBA( '--color-txt-primary', 1)
         font-weight: 700
         text-decoration: none
         display: flex
@@ -97,6 +120,15 @@ const globalNavItem = computed(() => globalNavItems.value)
         padding: 8px 24px
         background: $white
         border-radius: 24px
+        @include transition()
+        &:hover,
+        &:active,
+        &focus
+          color: CSStoRGBA( '--color-link', 1)
+          background: CSStoRGBA( '--color-link-low', 1)
+        @media ( prefers-color-scheme: dark )
+          background: $white
+          background: CSStoRGBA( '--color-bg-middle', 1)
 .size-20
   font-size: 20px
 .size-48
---
+++ components/global/PageToTop.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+const pageTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
+</script>
+
+<template>
+  <nav class="paget-to-top">
+    <div class="container">
+      <div class="paget-to-top__content">
+        <button
+          class="paget-to-top__btn"
+          @click="pageTop"
+        >
+          Page top
+          <span class="material-symbols-rounded size-20">
+            arrow_upward_alt
+          </span>
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style lang="sass" scoped>
+.paget-to-top
+  &__content
+    border-top: 1px solid CSStoRGBA( '--color-border-divider', 1)
+    padding: var(--scale-7) 0
+  &__btn
+    display: flex
+    align-items: center
+    gap: 8px
+    background: none
+    color: CSStoRGBA( '--color-theme-primary', 1)
+
+.size-20
+  font-size: 20px
+</style>
