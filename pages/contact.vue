457ef44b10af8441951ae201ceb5b26a8e724a77
masahiro <36883559+s18kitagawa@users.noreply.github.com>
2023-10-13 21:47:34 +0900

    update : form

--- pages/contact.vue
+++ pages/contact.vue
@@ -18,6 +18,7 @@
           name="contact"
           method="POST"
           data-netlify="true"
+          data-netlify-honeypot="bot-field"
         >
           <input type="hidden" name="form-name" value="contact" />
 
